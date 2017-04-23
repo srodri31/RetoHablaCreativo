2d91c57405f57781252dba5dd0e6cf995482ea2f
srodri31 <srodri31@eafit.edu.co>
2017-04-23 00:58:24 -0500

    changing form about population

--- src/app/poblacion/poblacion.component.ts
+++ src/app/poblacion/poblacion.component.ts
@@ -11,6 +11,20 @@ export class PoblacionComponent implements OnInit {
 
   poblacion = [];
   sortDesc: boolean = false;
+  isAddingNewRecord: boolean = false;
+  
+  add1993pop: boolean = false;
+  add2005pop: boolean = false;
+  add2006pop: boolean = false;
+  add2007pop: boolean = false;
+  add2008pop: boolean = false;
+  add2009pop: boolean = false;
+  add2010pop: boolean = false;
+  add2011pop: boolean = false;
+  add2012pop: boolean = false;
+  add2013pop: boolean = false;
+  add2014pop: boolean = false;
+  add2015pop: boolean = false;
 
   constructor(private poblacionService: PoblacionService,
     private arrayServices: ArrayServicesService) { }
@@ -33,6 +47,21 @@ export class PoblacionComponent implements OnInit {
 
   addNewData(data){
     this.poblacion.push(data);
+    
+    this.isAddingNewRecord = !this.isAddingNewRecord;
+    this.add1993pop = false;
+    this.add2005pop = false;
+    this.add2006pop = false;
+    this.add2007pop = false;
+    this.add2008pop = false;
+    this.add2009pop = false;
+    this.add2010pop = false;
+    this.add2011pop = false;
+    this.add2012pop = false;
+    this.add2013pop = false;
+    this.add2014pop = false;
+    this.add2015pop = false;
+
   }  
 
 }
